1216bc9d881cf8832a88ad8c8b9c62411401a8a5
TaQuanMinhLong <tqml2013@gmail.com>
2025-03-10 00:44:21 +0700

    comment

--- index.ts
+++ index.ts
@@ -3,3 +3,5 @@ import { PdfRenderer } from "./gen-main";
 const renderer = new PdfRenderer("./templates");
 const id = await renderer.render("app.vue", { message: "Hello" });
 console.log(id);
+
+// TODO read hook install browser when install package
